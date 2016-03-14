938a30e57701a7e0bc3cbc896c1d4fe1b4841b8a
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-03-14 16:32:27 -0700

    Changed Avatar border color to light gray color.

--- Traveler/Utility/ImageHelper.swift
+++ Traveler/Utility/ImageHelper.swift
@@ -12,9 +12,10 @@ import UIKit
 class ImageHelper {
 
     func roundImageView (imageView: UIImageView) {
-        imageView.layer.borderWidth     = 1.0
+        imageView.layer.borderWidth     = 0.5
         imageView.layer.cornerRadius    = imageView.frame.size.width/2
-        imageView.layer.borderColor     = UIColor(red: 44/255, green: 53/255, blue: 59/255, alpha: 1).CGColor
+//        imageView.layer.borderColor     = UIColor(red: 44/255, green: 53/255, blue: 59/255, alpha: 1).CGColor
+        imageView.layer.borderColor     = UIColor.lightGrayColor().CGColor
         imageView.layer.masksToBounds   = true
     }
 }
