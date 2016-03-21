a54dfb87fa3d8a933a940084e47cf68681e2844e
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-03-21 15:59:01 -0700

    Removed 10 seconds removal of ActivityIndicator.

--- Traveler/TRActivityIndicatorView.swift
+++ Traveler/TRActivityIndicatorView.swift
@@ -12,10 +12,6 @@ import UIKit
 
 class TRActivityIndicatorView: UIView {
 
-    private let TIMER_INTERVAL: Double = 10
-    private var timer: NSTimer = NSTimer()
-    private var parentViewController: TRBaseViewController?
-    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var activityIndicatorBGImage: UIImageView!
     @IBOutlet var activityIndicatorTopConstraint: NSLayoutConstraint!
@@ -30,10 +26,6 @@ class TRActivityIndicatorView: UIView {
         let window = appDelegate.window
         self.frame = (window?.bounds)!
         window?.addSubview(self)
-
-        
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(TIMER_INTERVAL, target: self, selector: "animationTimer", userInfo: nil, repeats: false)
-        self.activityIndicator?.startAnimating()
         
         if withClearBackGround {
             self.activityIndicatorBGImage.alpha = 0.0
@@ -46,23 +38,7 @@ class TRActivityIndicatorView: UIView {
     
     func stopActivityIndicator () {
         
-        self.timer.invalidate()
         self.activityIndicator?.stopAnimating()
         self.removeFromSuperview()
-    }
-    
-    func animationTimer() {
-        if ((self.activityIndicator?.isAnimating()) != nil) {
-
-            TRApplicationManager.sharedInstance.addErrorSubViewWithMessage("No Server Response")
-            
-//            self.parentViewController?.displayAlertWithTitle("No Server Response", complete: { (complete) -> () in
-//                self.stopActivityIndicator()
-//                self.removeFromSuperview()
-//                self.timer.invalidate()
-//                self.parentViewController = nil
-//            })
-            
-        }
     }
 }
