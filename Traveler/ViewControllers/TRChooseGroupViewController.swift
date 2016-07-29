4c63808df03523d08c3640f249a64c78474c387e
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-07-29 12:00:47 -0700

    Freelance Lobby should say 0 "activities" (not 0 activity)

--- Traveler/ViewControllers/TRChooseGroupViewController.swift
+++ Traveler/ViewControllers/TRChooseGroupViewController.swift
@@ -122,7 +122,7 @@ class TRChooseGroupViewController: TRBaseViewController, UITableViewDataSource, 
         
         if let eventCount = self.selectedGroup?.eventCount {
             self.selectedGroupViewEventCount.textColor = eventCount <= 0 ? UIColor.lightGrayColor() : UIColor(red: 255/255, green: 198/255, blue: 0/255, alpha: 1)
-            self.selectedGroupViewEventCount.text = eventCount > 1 ? eventCount.description + " Activities" : eventCount.description + " Activity"
+            self.selectedGroupViewEventCount.text = eventCount.description + " Activities"
         } else {
             self.selectedGroupViewEventCount.hidden = true
         }
