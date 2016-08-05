bb8152f61675c528fef4c9ae874de39d8b623cd0
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-08-04 19:37:14 -0700

    Revered back to dev

--- Traveler/TRConstants.swift
+++ Traveler/TRConstants.swift
@@ -36,8 +36,7 @@ struct K {
             static let TR_BaseUrl            =   "https://travelerbackend.herokuapp.com"
             static let TR_FIREBASE_DEFAULT   =   "https://crossroadsapp-dev.firebaseio.com/"
         #else
-            //static let TR_BaseUrl            =   "https://travelerbackend.herokuapp.com"
-        static let TR_BaseUrl            =   "https://live.crossroadsapp.co"
+            static let TR_BaseUrl            =   "https://travelerbackend.herokuapp.com"
             static let TR_FIREBASE_DEFAULT   =   "https://crossroadsapp-dev.firebaseio.com/"
         #endif
         
