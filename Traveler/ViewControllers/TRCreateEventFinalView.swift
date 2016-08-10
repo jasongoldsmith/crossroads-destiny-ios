b1653e671aecdf15cfa2074b7161f65ba7891261
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-08-10 10:45:15 -0700

    TR Create Event Final ViewController's, some UI elements added

--- Traveler/ViewControllers/TRCreateEventFinalView.swift
+++ Traveler/ViewControllers/TRCreateEventFinalView.swift
@@ -14,14 +14,59 @@ class TRCreateEventFinalView: TRBaseViewController {
     
     @IBOutlet weak var activityBGImageView: UIImageView!
     @IBOutlet weak var activityIconView: UIImageView!
+    @IBOutlet weak var activityNameLabel: UILabel!
+    @IBOutlet weak var activityLevelLabel: UILabel!
     
+   
     //Activities of same sub-typeJ
+    var selectedActivity: TRActivityInfo?
     lazy var activityInfo: [TRActivityInfo] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let _ = self.activityInfo.first {
+            self.updateViewWithActivity(self.activityInfo.first!)
+        }
+    }
+    
+    func updateViewWithActivity (activityInfo: TRActivityInfo) {
         
+        // Update default selected activity
+        self.selectedActivity = activityInfo
+        
+        // Update View
+        if let activitySubType = activityInfo.activitySubType {
+            self.activityNameLabel.text = activitySubType
+        }
+        
+        if let level = activityInfo.activityLevel where Int(level) > 0 {
+            let activityLavelString: String = "LEVEL \(level) "
+            let stringFontAttribute = [NSFontAttributeName: UIFont(name: "HelveticaNeue", size: 12)!]
+            let levelAttributedStr = NSAttributedString(string: activityLavelString, attributes: stringFontAttribute)
+            let finalString: NSMutableAttributedString = levelAttributedStr.mutableCopy() as! NSMutableAttributedString
+            
+            if let activityLight = activityInfo.activityLight  where Int(activityLight) > 0 {
+                let activityAttrString = NSMutableAttributedString(string: "Recommended Light: ")
+                let activityColorString = NSMutableAttributedString(string: "\u{02726} \(activityLight)")
+                activityColorString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 255/255, green: 198/255, blue: 0/255, alpha: 1) , range: NSMakeRange(0, activityColorString.length))
+                
+                activityAttrString.appendAttributedString(activityColorString)
+                finalString.appendAttributedString(activityAttrString)
+            }
+            
+            self.activityLevelLabel.attributedText = finalString
+            
+            if let _ = activityInfo.activityIconImage {
+                let imageUrl = NSURL(string: activityInfo.activityIconImage!)
+                self.activityIconView.sd_setImageWithURL(imageUrl)
+            }
+            
+            self.activityLevelLabel.layer.masksToBounds = true
+            self.activityLevelLabel.layer.borderColor = UIColor.whiteColor().CGColor
+            self.activityLevelLabel.layer.borderWidth = 1.0
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -30,6 +75,13 @@ class TRCreateEventFinalView: TRBaseViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    
+    @IBAction func addActivityButtonClicked (sender: UIButton) {
+        guard let _ = self.selectedActivity else {
+            return
+        }
     }
     
     @IBAction func cancelButtonPressed (sender: UIButton) {
