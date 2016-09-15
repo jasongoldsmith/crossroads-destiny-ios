a8f1fdc30f20149a5e703ca319e7ee7e31f9a458
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2016-09-14 23:02:49 -0700

    Bungie verification view controller flow removed.

--- Traveler/ViewControllers/TRRootViewController.swift
+++ Traveler/ViewControllers/TRRootViewController.swift
@@ -34,48 +34,45 @@ class TRRootViewController: TRBaseViewController {
 
     func loadAppInitialViewController () {
         if (TRUserInfo.isUserLoggedIn()) {
-            if (TRUserInfo.isUserVerified() == ACCOUNT_VERIFICATION.USER_VERIFIED.rawValue) {
-                
-                let userDefaults = NSUserDefaults.standardUserDefaults()
-                let userInfo = TRUserInfo()
-                userInfo.userName = userDefaults.objectForKey(K.UserDefaultKey.UserAccountInfo.TR_UserName) as? String
-                userInfo.password = userDefaults.objectForKey(K.UserDefaultKey.UserAccountInfo.TR_UserPwd) as? String
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            let userInfo = TRUserInfo()
+            userInfo.userName = userDefaults.objectForKey(K.UserDefaultKey.UserAccountInfo.TR_UserName) as? String
+            userInfo.password = userDefaults.objectForKey(K.UserDefaultKey.UserAccountInfo.TR_UserPwd) as? String
 
-                let createRequest = TRAuthenticationRequest()
-                createRequest.loginTRUserWith(userInfo) { (value ) in
-                    if value == true {
+            let createRequest = TRAuthenticationRequest()
+            createRequest.loginTRUserWith(userInfo) { (value ) in
+                if value == true {
+                    
+                    _ = TRGetEventsList().getEventsListWithClearActivityBackGround(true, clearBG: true, indicatorTopConstraint: self.ACTIVITY_INDICATOR_TOP_CONSTRAINT, completion: { (didSucceed) -> () in
                         
-                        _ = TRGetEventsList().getEventsListWithClearActivityBackGround(true, clearBG: true, indicatorTopConstraint: self.ACTIVITY_INDICATOR_TOP_CONSTRAINT, completion: { (didSucceed) -> () in
-                            
-                            var showEventListLandingPage = false
-                            var showGroups = false
-                            
-                            if(didSucceed == true) {
-                                let userDefaults = NSUserDefaults.standardUserDefaults()
-                                let shownGroupBool = userDefaults.boolForKey(K.UserDefaultKey.SHOWN_GROUP_PICKER)
-                                if (shownGroupBool == true) {
-                                    showEventListLandingPage = true
-                                } else if TRApplicationManager.sharedInstance.eventsList.count > 0 {
-                                    showGroups = true
-                                    userDefaults.setBool(true, forKey: K.UserDefaultKey.SHOWN_GROUP_PICKER)
-                                }
-                                
-                                TRApplicationManager.sharedInstance.addSlideMenuController(self, pushData: self.pushNotificationData, branchData: self.branchLinkData, showLandingPage: showEventListLandingPage, showGroups: showGroups)
-                                
-                                self.pushNotificationData = nil
-                            } else {
-                                self.appManager.log.debug("Failed")
+                        var showEventListLandingPage = false
+                        var showGroups = false
+                        
+                        if(didSucceed == true) {
+                            let userDefaults = NSUserDefaults.standardUserDefaults()
+                            let shownGroupBool = userDefaults.boolForKey(K.UserDefaultKey.SHOWN_GROUP_PICKER)
+                            if (shownGroupBool == true) {
+                                showEventListLandingPage = true
+                            } else if TRApplicationManager.sharedInstance.eventsList.count > 0 {
+                                showGroups = true
+                                userDefaults.setBool(true, forKey: K.UserDefaultKey.SHOWN_GROUP_PICKER)
                             }
-                        })
-                    } else{
-                        
-                        //Delete the saved Password if sign-in was not successful
-                        userDefaults.setValue(nil, forKey: K.UserDefaultKey.UserAccountInfo.TR_UserPwd)
-                        userDefaults.synchronize()
-                        
-                        // Add Error View
-                        TRApplicationManager.sharedInstance.addErrorSubViewWithMessage("The username and password do not match. Please try again.")
-                    }
+                            
+                            TRApplicationManager.sharedInstance.addSlideMenuController(self, pushData: self.pushNotificationData, branchData: self.branchLinkData, showLandingPage: showEventListLandingPage, showGroups: showGroups)
+                            
+                            self.pushNotificationData = nil
+                        } else {
+                            self.appManager.log.debug("Failed")
+                        }
+                    })
+                } else{
+                    
+                    //Delete the saved Password if sign-in was not successful
+                    userDefaults.setValue(nil, forKey: K.UserDefaultKey.UserAccountInfo.TR_UserPwd)
+                    userDefaults.synchronize()
+                    
+                    // Add Error View
+                    TRApplicationManager.sharedInstance.addErrorSubViewWithMessage("The username and password do not match. Please try again.")
                 }
             }
         } else {
